ae632d5b371a020c93b0e4d7cc913d8128c80db1
Eric Veciana <eveciana21@gmail.com>
2022-09-11 14:27:01 -0600

    created player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,16 +4,31 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    [SerializeField] private float _speed = 2.5f;
+
     void Start()
     {
         transform.position = new Vector3(0, 0, 0);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.right * 5 * Time.deltaTime);
+
+        float horizontal = Input.GetAxis("Horizontal");
+        //new Vector3 (1,0,0) * 0 * 3.5f * real time
+        //transform.Translate(Vector3.right * horizontal * _speed * Time.deltaTime);
+
+        float vertical = Input.GetAxis("Vertical");
+        //new Vector3 (0,1,0) * 0 * 3.5f * real time
+        //transform.Translate(Vector3.up * vertical * _speed * Time.deltaTime);
+
+        //transform.Translate(new Vector3(horizontal, vertical, 0) * _speed * Time.deltaTime);
+
+        Vector3 direction = new Vector3 (horizontal, vertical, 0);
+        transform.Translate(direction * _speed * Time.deltaTime);
+        
     }
-    
+
+      
 }
